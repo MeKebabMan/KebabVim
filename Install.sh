e9f53a2c7adfbec313aa8ba4f91360a7d333f2de
Me Kebab Man <151750810+MeKebabMan@users.noreply.github.com>
2024-07-14 14:47:57 +0200

    Delete Install.sh

--- Install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-test -d ~/.config/nvim/* && rm -rf ~/.config/nvim/* && \
-	mkdir -p ~/.config/nvim/ && git clone https://github.com/MeKebabMan/KebabVim ~/.config/nvim/ && \
-		nvim ~/.config/nvim/NVIM_README.txt
