ba471113f95a89b08c8b7c921faecab324a69a29
MeKebabMan <lawenqader2010@gmail.com>
2024-07-21 00:43:01 +0200

    Fixed auto update

--- init.lua
+++ init.lua
@@ -83,7 +83,7 @@ if directory_exists(vim.fn.expand(tostring(vim.g.KebabVimPath))) then
 		plugin_module.which_key()
 
 		if vim.g.AutoUpdate then
-			if KebabVim_utils.UPDATE() then
+			if not KebabVim_utils.UPDATE() then
 				vim.notify("Failed to do a update check!", vim.log.levels.ERROR)
 			end
 		end
