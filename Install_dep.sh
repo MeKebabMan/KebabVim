f2de746e7692675a5b6b2e929763dd61e061fa7b
MeKebabMan <lawenqader2010@gmail.com>
2024-07-21 12:18:28 +0200

    Git added

--- Install_dep.sh
+++ Install_dep.sh
@@ -10,16 +10,49 @@
 NODE_VERSION=22
 DOTNET_VERSION=8.0
 
+GRANTED=false
+
 echo "======================| KEBABVIM |==========================="
 
 # Get user permissions!
 read -p "Sudo permissions are required for this bash script. Allow sudo permissions? (Y/N): " Perms
+echo "WARNING: DEFAULT IS (NO)"
 if [[ ! $Perms =~ ^[Yy][Ee]?[Ss]?$ ]]; then 
 	echo "Permission's denied unable to go forward! exit 0"
 	exit 0
 fi
 
 echo "Permission's granted installing dependencies!"
+
+# INSTALL GIT 
+if ! command -v git &> /dev/null; then 
+	
+	if [ -f /etc/redhat-release ]; then 
+
+		sudo dnf install git -y
+
+	elif [ -f /etc/lsb-release ]; then 
+
+		sudo apt-get install git -y
+
+	elif [ -f /etc/debian_version ]; then
+
+		sudo apt-get install git -y 
+
+	elif [ -f /etc/arch-release ]; then
+
+		sudo pacman -Syu git --noconfirm
+
+	else 
+		echo "UNSUPPORTED DISTRO! UNABLE TO INSTALL CURL. PLEASE INSTALL CURL!"
+		exit 1
+	fi
+
+	if ! command -v git &> /dev/null; then 
+		echo "Failed to install git!"
+		exit 1
+	fi
+fi 
 
 # INSTALL CURL 
 if ! command -v curl &> /dev/null; then 
